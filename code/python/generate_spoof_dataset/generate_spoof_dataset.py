dcf987ec6fca20964f82d7ab5ff5e70780373b1f
AleksanderTidemann <aleksandertid@gmail.com>
2020-09-24 12:41:33 +0200

    Added small program that can generate spoof dataset from a desired directory of files into .csv

---
+++ code/python/generate_spoof_dataset/generate_spoof_dataset.py
@@ -0,0 +1,27 @@
+import os
+import random
+import pandas as pd
+
+os.chdir('C:\\Users\\aleks\\Desktop\\test\\tracks')
+path_to_csv = os.path.dirname(os.path.abspath(__file__))
+emo_list_length = 5
+emo_int_range = 10
+
+spoof_data = pd.DataFrame()
+
+def convert(x): 
+    string_list = [str(i) for i in x] 
+    list_merged = int("".join(string_list))   
+    return(list_merged) 
+
+for f in os.listdir():
+    emo_tags = []
+    file_name, file_ext = os.path.splitext(f)
+    for i in range(emo_list_length):
+        emo_tags.append(random.randint(1, emo_int_range))
+
+    #Merge the emo tags list to one integer and add it to a new row in the dataframe.
+    new_row = {'track_name': file_name, 'tags': convert(emo_tags)}
+    spoof_data = spoof_data.append(new_row, ignore_index=True)
+
+spoof_data.to_csv(path_to_csv + '\\spoof_data.csv', index=False)
